f9b5d9b71b6894663552f1beb79db58035751824
Fatikha azzahra A <fatikhaazzahraa@192.168.1.3>
2020-06-15 19:10:08 +0700

    cek pembimbing

--- application/controllers/backend/admin/Pembimbing.php
+++ application/controllers/backend/admin/Pembimbing.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pembimbing extends CI_Controller
+class pembimbing magang  extends CI_Controller
 {
     public function __construct()
     {
